709b11ced22eae01ae8f704270d32c4748c9b8db
서주현 <beloved930223@gmail.com>
2022-03-16 23:41:16 +0900

    setting server and home page

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
-console.log('hello')
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname+ "/public"));
+//route
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+
+app.listen(3000, ()=> console.log('hello'));
