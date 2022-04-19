327c455a42da7b2e220b7441c7419b6ec9f29668
서주현 <beloved930223@gmail.com>
2022-04-19 23:52:32 +0900

    feat: room notifications

--- src/server.js
+++ src/server.js
@@ -1,28 +1,35 @@
-import http from "http";
-import SocketIO from "socket.io";
-import express from "express";
+import http from 'http';
+import SocketIO from 'socket.io';
+import express from 'express';
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
 //route
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 
 const httpServer = http.createServer(app);
 /** localhost:3000/socket.io/socket.io.js */
 const wss = SocketIO(httpServer);
 
-wss.on("connection", (socket) => {
+wss.on('connection', (socket) => {
   socket.onAny((event) => {
     console.log(`Socket event: ${event}`);
   });
-  socket.on("enter_room", (roomName, done) => {
+  socket.on('enter_room', (roomName, done) => {
     socket.join(roomName);
-    socket.to(roomName).emit("welcome"); //room에 있는 모든 사람들에게 emit
     done();
+    socket.to(roomName).emit('welcome'); //room에 있는 모든 사람들에게 emit
+  });
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) => socket.to(room).emit('bye'));
+  });
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', msg);
+    done(); //프론트엔드에서 코드를 실행함.
   });
 });
 
@@ -48,4 +55,4 @@ wss.on("connection", (socket) => {
 //   });
 // });
 
-httpServer.listen(3000, () => console.log("server listen 3000"));
+httpServer.listen(3000, () => console.log('server listen 3000'));
